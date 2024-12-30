fe499baa15654bc192019d575197dcd48cbb4f22
unknown <fiacregiraneza@gmail.com>
2024-12-30 10:03:07 +0200

    step_18

--- lib/services/crud/notes_services.dart
+++ lib/services/crud/notes_services.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/foundation.dart';
+import 'package:mypersonalnotes/extensions/list/filter.dart';
 import 'package:mypersonalnotes/services/crud/crud_exceptions.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
@@ -11,23 +12,42 @@ class NotesService {
 
   List<DatabaseNote> _notes = [];
 
+  DatabaseUser? _user;
+
   static final NotesService _shared = NotesService._sharedInstance();
   NotesService._sharedInstance() {
     _notesStreamController = StreamController<List<DatabaseNote>>.broadcast(
-      onListen: () => {_notesStreamController.add(_notes)},
+      onListen: () => {_notesStreamController.sink.add(_notes)},
     );
   }
   factory NotesService() => _shared;
 
   late final StreamController<List<DatabaseNote>> _notesStreamController;
-  Stream<List<DatabaseNote>> get allNotes => _notesStreamController.stream;
+  Stream<List<DatabaseNote>> get allNotes =>
+      _notesStreamController.stream.filter((note) {
+        final currentUser = _user;
+        if (currentUser != null) {
+          return note.userId == currentUser.id;
+        } else {
+          throw UserShouldBeSetBeforeReadAllNotesException();
+        }
+      });
 
-  Future<DatabaseUser> getOrCreateUser({required String email}) async {
+  Future<DatabaseUser> getOrCreateUser({
+    required String email,
+    bool setAsCurrentUser = true,
+  }) async {
     try {
       final user = await getUser(email: email);
+      if (setAsCurrentUser) {
+        _user = user;
+      }
       return user;
     } on CouldNotFindUser {
       final createdUser = await createUser(email: email);
+      if (setAsCurrentUser) {
+        _user = createdUser;
+      }
       return createdUser;
     } catch (e) {
       rethrow;
@@ -109,10 +129,15 @@ class NotesService {
     await getNote(id: note.id);
 
     // update db
-    final updatesCount = await db.update(noteTable, {
-      textColumn: text,
-      isSyncedWithCloudColumn: 0,
-    });
+    final updatesCount = await db.update(
+      noteTable,
+      {
+        textColumn: text,
+        isSyncedWithCloudColumn: 0,
+      },
+      where: 'id=?',
+      whereArgs: [note.id],
+    );
     if (updatesCount == 0) {
       throw CouldNotUpdateNote();
     } else {
@@ -154,7 +179,6 @@ class NotesService {
       text: text,
       isSyncedWithCloud: true,
     );
-    
 
     // Only add to the list if it doesn't already exist
     if (_notes.every((existingNote) => existingNote.id != note.id)) {
@@ -301,10 +325,11 @@ class DatabaseNote {
     return DatabaseNote(
       id: id ?? this.id,
       text: text ?? this.text,
-      userId: userId ?? this.userId, 
+      userId: userId ?? this.userId,
       isSyncedWithCloud: true,
     );
   }
+
   DatabaseNote.fromRow(Map<String, Object?> map)
       : id = map[idColumn] as int,
         userId = map[userIdColumn] as int,
